d8650fc860faab96c2e77e36c601462cc4d0c63a
Alireza Shateri <alirezashateri7@gmail.com>
2024-10-11 19:23:50 -0400

    :hammer: chore: make lineitemorder join hashjoin in spja

--- tornado-api/src/main/java/uk/ac/manchester/tornado/api/TornadoExecutionPlan.java
+++ tornado-api/src/main/java/uk/ac/manchester/tornado/api/TornadoExecutionPlan.java
@@ -128,7 +128,7 @@ public class TornadoExecutionPlan implements AutoCloseable {
             // processBuilder.redirectError(ProcessBuilder.Redirect.INHERIT);
             // processBuilder.redirectOutput();
             p = processBuilder.start();
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             System.out.println("Intel PCM execution failed: " + e.getMessage());
         }
 
